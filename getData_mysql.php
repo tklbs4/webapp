d8d3862437ab013459b60df8c6f811bdbc034e8b
tklbs4 <124056694+tklbs4@users.noreply.github.com>
2023-01-31 15:39:38 +0100

    Update getData_mysql.php

--- getData_mysql.php
+++ getData_mysql.php
@@ -1,15 +1,13 @@
-<?php 
-    $server = "klettner.database.windows.net";
-    $database = "faketaxi";
-    $username = "thomas.klettner";
-    $password = "TelefonTelefon69!";
-
-    
+<?php   
     try{
-        $database = "FakeTaxi";  
-        $connection = new PDO("dblib:version=8.0;charset=UTF-8;host={$server};dbname={$database}", $username, $password);  
-        // EXCEPTION konfigurieren
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $serverName = "klettner.database.windows.net"; // update me
+        $connectionOptions = array(
+        "Database" => "FakeTaxi", // update me
+        "Uid" => "thomas.klettner", // update me
+        "PWD" => "TelefonTelefon69!" // update me
+        );
+        //Establishes the connection
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
 
         
         $items = ([$_POST["username"], $_POST["firstname"], $_POST["lastname"], $_POST["birthdate"], $_POST["email"], $_POST["password"], 
@@ -19,14 +17,14 @@
 
         $sql = "INSERT INTO users (username, password, firstname, lastname, birthdate, email, street, city, postcode, state, phone)
         VALUES ('$items[0]', '$items[5]', '$items[1]', '$items[2]', '$items[3]', '$items[4]', '$items[6]', '$items[7]', '$items[8]', '$items[9]', '$items[10]')";
-        $connection->exec($sql);
+        sqlsrv_query($conn, $sql);
         echo ('Eintrag erfolgreich');
     }
     catch(PDOException $e)
     {
         echo ( 'Fehler: ' . $e->getMessage());
     }
-    $connection = null;
+    $conn = null;
        
     header("Location: website_example.php");
 ?>
